292144130bc89e694c46191fbc55ab42442d0c5a
pritesh.mahajan <pritesh.mahajan@infosys.com>
2022-06-17 18:32:36 +0530

    commited

--- hello.sh
+++ hello.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 git init
-git checkout -b demo
+git checkout -b demo12
 git add .
 git commit -m "commited"
 
 git remote add $i https://github.com/pritesh3698/practice4.git
-  git push -u $idemo
+  git push -u $idemo12
 
-git push --set-upstream origin 
+git push --set-upstream origin
