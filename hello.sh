9839f5bae296f3ef52a30ecb1e85910fa239d95b
pritesh.mahajan <pritesh.mahajan@infosys.com>
2022-06-17 17:04:23 +0530

    commited

---
+++ hello.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+git init
+git checkout -b demo
+git add .
+git commit -m "commited"
+
+git remote add $i https://github.com/pritesh3698/practice4.git
+  git push -u $idemo
+
+git push --set-upstream origin
